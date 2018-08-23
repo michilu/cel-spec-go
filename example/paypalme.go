690d8b010552dd65845dee6e25efd4cb0b979016
ENDOH takanao <djmchl@gmail.com>
2018-08-23 19:37:59 +0700

    add ‘go generate’ to example

--- example/paypalme.go
+++ example/paypalme.go
@@ -1,5 +1,7 @@
 package example
 
+//go:generate cel-spec-go gen
+
 type (
 	// Paypalme returns a new URL that generates by given params.
 	Paypalme func(username string, amount string, currency string) string
